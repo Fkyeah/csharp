44ec84d8a1c7dbef03efbbb1c87ae6a0967b1d84
Fkyeah <dimx63@mail.ru>
2020-11-12 17:51:52 +0300

    add thirth homework

---
+++ thirth_homework/OddNumbers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class OddNumbers
+{
+    public int getSum(View view)
+    {
+        int digit = 0;
+        int sum = 0;
+        do
+        {
+            digit = view.getInt("Введите число.");
+            if (digit % 2 != 0 && digit > 0) sum += digit;
+        } while (digit != 0);
+        return sum;
+    }
+}
+
