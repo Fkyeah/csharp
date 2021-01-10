d6d4e0e6a06e4ab62b5a8bed4aa91674a91286c7
Fkyeah <dimx63@mail.ru>
2021-01-10 15:53:02 +0300

    add firth homework

---
+++ C-sharp level two/fourth_homework/Asteroids/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Asteroids
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form form = new Form();
+            form.Width = 1024;
+            form.Height = 768;
+            form.Show();
+            Game.Init(form);
+            Game.Draw();
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ C-sharp level two/fourth_homework/Lists/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2. Дана коллекция List<T>, требуется подсчитать, сколько раз каждый элемент встречается в данной коллекции:
+            // а) для целых чисел;
+            ArrayList listNonGeneric = new ArrayList() { 1, 4, 3, 5, 4, 7, 6, 5, 4, 5, 3, 1, 8, 9, 8, 4 };
+            int[] sortArr = new int[9];
+            foreach (int val in listNonGeneric)
+            {
+                Console.Write($"{val} ");
+                sortArr[val - 1]++;
+            }
+            Console.WriteLine();
+            listNonGeneric.Sort();
+            foreach (int val in listNonGeneric)
+            {
+                Console.Write($"{val} ");
+            }
+            Console.WriteLine();
+            for (int j = 1; j < sortArr.Length; j++)
+            {
+                if (sortArr[j] == 0) continue;
+                Console.WriteLine($"Число {j}  встречается {sortArr[j]} раз");
+            }
+            // б) *для обобщенной коллекции;
+            // в) *используя Linq.
+            Console.WriteLine("Для обобщенной коллекции с помощью LINQ");
+            List<int> listGeneric = new List<int>() { 1, 4, 3, 5, 4, 7, 6, 5, 4, 5, 3, 1, 8, 9, 8, 4 };
+            IEnumerable<int> distNum = listGeneric.Distinct().OrderBy(value => value);
+            foreach(var num in distNum)
+            {
+                Console.WriteLine($"Число {num} встречается {listGeneric.Where(val => val == num).Count()} раз");
+            }
+            Console.ReadLine();
+        }        
+    }
+}
