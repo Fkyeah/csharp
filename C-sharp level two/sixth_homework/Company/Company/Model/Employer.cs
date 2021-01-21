4e6fb1c97e828f2eebe728822daae4c6c1b90ee1
Fkyeah <dimx63@mail.ru>
2021-01-21 11:53:13 +0300

    sixth_homework

---
+++ C-sharp level two/sixth_homework/Company/Company/Model/Employer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company
+{
+    public class Employer : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private int _id;
+        public int Id 
+        { 
+            get 
+            {
+                return _id;
+            } 
+            set 
+            {
+                _id = value;
+            } 
+        }
+
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Name"));
+            }
+        }
+        private string _lastName;
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("LastName"));
+            }
+        }
+        public Department department { get; set; }
+        public Employer(int id, string name, string lastName, Department department)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.LastName = lastName;
+            this.department = department;
+        }
+        public Employer()
+        {
+        }
+    }
+}
---
+++ C-sharp level two/sixth_homework/Company/Company/View/AddDepWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Company
+{
+    /// <summary>
+    /// Логика взаимодействия для AddDepWindow.xaml
+    /// </summary>
+    public partial class AddDepWindow : Window
+    {
+        public Department Dep { get; private set; }
+        public ObservableCollection<Department> _deps;
+        public AddDepWindow(ObservableCollection<Department> departments)
+        {
+            InitializeComponent();
+            _deps = departments;
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Dep = new Department(_deps.Last().DepartId + 1, DepartmentTextBox.Text);
+            this.DialogResult = true;
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+    }
+}
