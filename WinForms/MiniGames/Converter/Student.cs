82372b7e4dbe66da373db57d5924bf599d09ae64
Fkyeah <dimx63@mail.ru>
2021-07-09 23:21:42 +0300

    add winforms

---
+++ WinForms/MiniGames/Converter/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+
+namespace Converter
+{
+    [Serializable]
+    public class Student
+    {
+        private string _firstName;
+        private string _secondName;
+        private string _univercity;
+        private string _faculty;
+        private int _age;
+        private int _course;
+        private string _group;
+        private string _city;
+        public string FirstName { get { return _firstName; } set { _firstName = value; } }
+        public string SecondName { get { return _secondName; } set { _secondName = value; } }
+        public string Univercity { get { return _univercity; } set { _univercity = value; } }
+        public string Faculty { get { return _faculty; } set { _faculty = value; } }
+        public int Age { get { return _age; } set { _age = value; } }
+        public int Course { get { return _course; } set { _course = value; } }
+        public string Group { get { return _group; } set { _group = value; } }
+        public string City { get { return _city; } set { _city = value; } }
+    }
+}
---
+++ WinForms/Readme.md
@@ -0,0 +1,4 @@
+MiniGames:
+1) Believe or Not Believe
+2) Converter CSV to XML
+3) DataBase Creator for Believe or Not Believe 
