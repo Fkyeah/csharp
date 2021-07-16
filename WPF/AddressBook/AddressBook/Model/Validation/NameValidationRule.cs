d6bb33b08732a349b77f48ffc2dceead09686bce
Fkyeah <dimx63@mail.ru>
2021-07-17 00:51:21 +0300

    update validation rule for name

--- WPF/AddressBook/AddressBook/Model/Validation/NameValidationRule.cs
+++ WPF/AddressBook/AddressBook/Model/Validation/NameValidationRule.cs
@@ -6,12 +6,12 @@ namespace AddressBook.Model.Validation
 {
     class NameValidationRule : ValidationRule
     {
-        Regex regex = new Regex(@"^[A-Za-zА-Яа-я][А-Яа-яa-zA-Z0-9-_\.]{2,50}$");
+        Regex regex = new Regex(@"^[A-Za-zА-Яа-я]{2,50}$");
         public override ValidationResult Validate(object value, CultureInfo cultureInfo)
         {
             string login = value.ToString();
             if (regex.IsMatch(login)) return new ValidationResult(true, null);
-            else return new ValidationResult(false, "Логин может состоять из цифр и букв с ограничением в 2-50 символов.\nПервый символ должен начинаться с буквы");
+            else return new ValidationResult(false, "Имя и фамилия могут состоять из букв с ограничением в 2-50 символов.\nПервый символ должен начинаться с буквы");
         }
     }
 }
